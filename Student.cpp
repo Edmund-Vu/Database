a83b2d64c7a90ea0dfdec9e113f5da7a2c43d016
Edmund <edmundvu816@gmail.com>
2020-05-12 08:13:39 +0000

    Overloaded constructors, etc. for student class

---
+++ Student.cpp
@@ -0,0 +1,56 @@
+#include "Student.h"
+
+Student::Student(){
+
+}
+
+Student::Student(int id){
+  this->id = id;
+}
+
+Student::~Student(){
+
+}
+
+Student::Student(string name, string level, string major, int adv, double gpa){
+  this->name = name;
+  this->level = level;
+  this->major = major;
+  this->adv = adv;
+  this->gpa = gpa;
+  id = rand()%5000+1000;
+}
+
+Student::Student(string name, string level, string major, int adv, int id, double gpa){
+this->name = name;
+this->level = level;
+this->major = major;
+this->adv = adv;
+this->gpa = gpa;
+this->id = id;
+}
+
+void Student::setAdv(int i){
+  adv = i;
+}
+
+ostream& operator<<(ostream& os, const Student& s){
+  os << s.name << '/' << s.level << '/' << s.major << '/' << s.adv << '/' << s.gpa << '/' << s.id;
+  return os;
+}
+
+bool operator> (const Student &s1, const Student &s2){
+  return s1.id > s2.id;
+}
+
+bool operator< (const Student &s1, const Student &s2){
+  return s1.id < s2.id;
+}
+
+bool operator== (const Student &s1, const Student &s2){
+  return s1.id == s2.id;
+}
+
+bool operator!= (const Student &s1, const Student &s2){
+  return s1.id != s2.id;
+}
